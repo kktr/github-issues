653a5bf077ebedac42063346405a1cbd933f6559
tomek <tomaszpawelkowalski@gmail.com>
2022-03-28 12:54:02 +0200

    Refactoring  search results.

--- pages/interfaces/search.ts
+++ pages/interfaces/search.ts
@@ -33,46 +33,46 @@ type Results = Result[];
 
 const result1: UserResult = {
   id: 1,
-  username: 'userOne',
-  avatar:
+  login: 'userOne',
+  avatarURL:
     'https://secure.gravatar.com/avatar/e7956084e75f239de85d3a31bc172ace?d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-user-420.png',
   fullName: 'User One',
-  description: 'lorem ipsium dipsum',
-  localization: 'Warszawa',
-  numberOfFollowers: 10,
-  numberOfFollowing: 20,
+  bio: 'lorem ipsium dipsum',
+  location: 'Warszawa',
+  followers: 10,
+  following: 20,
   starred: 10,
 };
 
 const result2: UserResult = {
   id: 2,
-  username: 'userTwo',
-  avatar:
+  login: 'userTwo',
+  avatarURL:
     'https://secure.gravatar.com/avatar/e7956084e75f239de85d3a31bc172ace?d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-user-420.png',
   fullName: 'User Two',
-  description: 'lorem ipsium dipsum 2',
-  localization: 'Poznań',
-  numberOfFollowers: 20,
-  numberOfFollowing: 21,
+  bio: 'lorem ipsium dipsum 2',
+  location: 'Poznań',
+  followers: 20,
+  following: 21,
   starred: 22,
 };
 
 const result3: RepositoryResult = {
   id: 3,
   name: 'repoOne',
-  starred: 30,
+  stargazersCount: 30,
   description: 'lorem motorem',
   programingLanguage: 'Python',
-  whenUpdated: '7 days ago',
+  updatedAt: '7 days ago',
 };
 
 const result4: RepositoryResult = {
   id: 4,
   name: 'repoTwo',
-  starred: 40,
+  stargazersCount: 40,
   description: 'lorem motorem potworem',
   programingLanguage: 'typescript',
-  whenUpdated: '22 aug 2016',
+  updatedAt: '22 aug 2016',
   issues: '7 issues needed help',
 };
 
