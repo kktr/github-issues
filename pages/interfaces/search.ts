8c1c371316baffb737d231572cff9dab51e55c1e
Krystian Kat <krystian.kat@protonmail.com>
2022-03-28 11:00:28 +0200

    feat: update repository result and add mock result

--- pages/interfaces/search.ts
+++ pages/interfaces/search.ts
@@ -20,10 +20,11 @@ export interface UserResult {
 export interface RepositoryResult {
   id: number;
   name: string;
-  starred: string;
+  starred: number;
   description: string;
   programingLanguage: string;
   whenUpdated: string;
+  issues?: string;
 }
 
 type Result = UserResult | RepositoryResult;
@@ -31,3 +32,50 @@ type Result = UserResult | RepositoryResult;
 export interface Results {
   results: Result[];
 }
+
+const result1: UserResult = {
+  id: 1,
+  username: 'userOne',
+  avatar:
+    'https://secure.gravatar.com/avatar/e7956084e75f239de85d3a31bc172ace?d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-user-420.png',
+  fullName: 'User One',
+  description: 'lorem ipsium dipsum',
+  localization: 'Warszawa',
+  numberOfFollowers: 10,
+  numberOfFollowing: 20,
+  starred: 10,
+};
+
+const result2: UserResult = {
+  id: 2,
+  username: 'userTwo',
+  avatar:
+    'https://secure.gravatar.com/avatar/e7956084e75f239de85d3a31bc172ace?d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-user-420.png',
+  fullName: 'User Two',
+  description: 'lorem ipsium dipsum 2',
+  localization: 'Poznań',
+  numberOfFollowers: 20,
+  numberOfFollowing: 21,
+  starred: 22,
+};
+
+const result3: RepositoryResult = {
+  id: 3,
+  name: 'repoOne',
+  starred: 30,
+  description: 'lorem motorem',
+  programingLanguage: 'Python',
+  whenUpdated: '7 days ago',
+};
+
+const result4: RepositoryResult = {
+  id: 4,
+  name: 'repoTwo',
+  starred: 40,
+  description: 'lorem motorem potworem',
+  programingLanguage: 'typescript',
+  whenUpdated: '22 aug 2016',
+  issues: '7 issues needed help',
+};
+
+const mockResults: Results = [result1, result2, result3, result4];
