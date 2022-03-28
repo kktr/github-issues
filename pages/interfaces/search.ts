92138dab0f7957f3e47e102106f0153e69c4b1ad
Krystian Kat <krystian.kat@protonmail.com>
2022-03-28 15:45:38 +0200

    feat: make mockResult less dumb

--- pages/interfaces/search.ts
+++ pages/interfaces/search.ts
@@ -34,12 +34,12 @@ export type Results = Result[];
 
 const result1: UserResult = {
   id: 1,
-  login: 'userOne',
+  login: 'MichalPaszkiewicz',
   avatarURL:
     'https://secure.gravatar.com/avatar/e7956084e75f239de85d3a31bc172ace?d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-user-420.png',
-  fullName: 'User One',
-  bio: 'lorem ipsium dipsum',
-  location: 'Warszawa',
+  fullName: 'Michal Paszkiewicz',
+  bio: 'Just a guy who likes to make things @MichalYouDoing',
+  location: 'London, England',
   followers: 10,
   following: 20,
   starred: 10,
@@ -47,12 +47,12 @@ const result1: UserResult = {
 
 const result2: UserResult = {
   id: 2,
-  login: 'userTwo',
+  login: 'michalinamuskala',
   avatarURL:
     'https://secure.gravatar.com/avatar/e7956084e75f239de85d3a31bc172ace?d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-user-420.png',
-  fullName: 'User Two',
-  bio: 'lorem ipsium dipsum 2',
-  location: 'Poznań',
+  fullName: 'Michalina Muskała ',
+  bio: 'Mój mąż? Mój mąż z zawodu jest dyrektorem!',
+  location: 'London',
   followers: 20,
   following: 21,
   starred: 22,
@@ -60,22 +60,23 @@ const result2: UserResult = {
 
 const result3: RepositoryResult = {
   id: 3,
-  name: 'repoOne',
+  name: '0bserver07/One-Hundred-Layers-Tiramisu',
   stargazersCount: 30,
-  description: 'lorem motorem',
-  programingLanguage: 'Python',
-  updatedAt: '7 days ago',
+  description:
+    'Keras Implementation of The One Hundred Layers Tiramisu: Fully Convolutional DenseNets for Semantic Segmentation by (…',
+  programingLanguage: 'Jupyter Notebook',
+  updatedAt: '13 days ago',
+  license: 'MIT',
 };
 
 const result4: RepositoryResult = {
   id: 4,
-  name: 'repoTwo',
-  stargazersCount: 40,
+  name: "Source code for 'Practical Oracle Cloud Infrastructure' by Michal Jakóbczyk",
+  stargazersCount: 7,
   description: 'lorem motorem potworem',
-  programingLanguage: 'typescript',
-  updatedAt: '22 aug 2016',
+  programingLanguage: 'HCL',
+  updatedAt: '11 Feb 2020',
   issues: '7 issues needed help',
-  license: 'MIT',
 };
 
 export const mockResults: Results = [result1, result2, result3, result4];
