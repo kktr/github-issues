31663a15da99c9838f287160c92f45aedb5db00f
tomek <tomaszpawelkowalski@gmail.com>
2022-03-28 15:05:52 +0200

    Added fn getting user details.

--- pages/api/main.ts
+++ pages/api/main.ts
@@ -14,12 +14,14 @@ export default async function handler(
   res: NextApiResponse 
 ) {
     try {
-        const Users = await getExternalUsersFromGitHub();
-      const Repositories = await getExternalRepositoriesAPIGitHub();
-      if (!Users || !Repositories) { throw new Error('User or Repo not found') };
-        const data= sendBasicData(Users, Repositories);
+      const Users = await getExternalUsersFromGitHub();
+      const FullUsers = await GetMoreUserData(Users);
+      // const Repositories = await getExternalRepositoriesAPIGitHub();
+
+      // if (!Users || !Repositories) { throw new Error('User or Repo not found') };
+        // const data= sendBasicData(Users, Repositories);
         // console.log(Users);
-        res.status(200).json({data});
+        res.status(200).json({FullUsers});
     } catch (err:any) {
         res.status(500).json({ message: err.message })
     }
@@ -28,7 +30,7 @@ export default async function handler(
 export async function getExternalUsersFromGitHub() {
 
   try {
-    const res = await axios.get('https://api.github.com/search/users', { params: { q: 'john', } });
+    const res = await axios.get('https://api.github.com/search/users', { params: { q: 'john', per_page:5} });
     const data: GeneralUserList = res.data;
     console.log(data);
     return  data as GeneralUserList;
@@ -50,38 +52,54 @@ export async function getExternalRepositoriesAPIGitHub():Promise<GeneralRepoList
     throw new Error(error);
   }
 }
+export async function  GetMoreUserData(BasicsUsers:GeneralUserList)  {
+  const users: UserGithub[] = []
+  try {
+    for (let user of BasicsUsers.items) {
+      const res = await axios.get('https://api.github.com/users/' + user.login);
+      const data: UserGithub = res.data;
+      console.log(data);
+      users.push(data);
+    } 
+    return users;
+  } catch (error: any) {
+    console.error(error);
+    throw new Error(error);
+  }
+}
+
 
   
-export  function sendBasicData(Users:GeneralUserList,Repository:GeneralRepoList):Result[] {
-  const data: Result[] = [];
-  Users.items.forEach((user:GeneralUser) => {
-    const result:Result = {
-      id: user.id,
-      login: user.login,
-      avatarURL: user.avatarURL,
-      // fullName: user.name,
-      // bio: user.bio,
-      // location: user.location,
-      // followers: user.followers,
-      // following: user.following,
-      starred: user.starredURL,
-    };
-    data.push(result);
-  });
-  Repository.items.forEach((repo:GeneralRepo) => {
-    const result:Result = {
-      id: repo.id,
-      name: repo.name,
-      // stargazersCount: repo.stargazers_count,
-      description: repo.description,
-      programingLanguage: repo.language,
-      // updatedAt: repo.updated_at,
-      // issues: repo.issues_url,
-      // license: repo.license,
-    };
-    data.push(result);
-  });
-  return data;
-}
+// export  function sendBasicData(Users:GeneralUserList,Repository:GeneralRepoList):Result[] {
+//   const data: Result[] = [];
+//   Users.items.forEach((user:GeneralUser) => {
+//     const result:Result = {
+//       id: user.id,
+//       login: user.login,
+//       avatarURL: user.avatarURL,
+//       // fullName: user.name,
+//       // bio: user.bio,
+//       // location: user.location,
+//       // followers: user.followers,
+//       // following: user.following,
+//       starred: user.starredURL,
+//     };
+//     data.push(result);
+//   });
+//   Repository.items.forEach((repo:GeneralRepo) => {
+//     const result:Result = {
+//       id: repo.id,
+//       name: repo.name,
+//       // stargazersCount: repo.stargazers_count,
+//       description: repo.description,
+//       programingLanguage: repo.language,
+//       // updatedAt: repo.updated_at,
+//       // issues: repo.issues_url,
+//       // license: repo.license,
+//     };
+//     data.push(result);
+//   });
+//   return data;
+// }
    
   
