d534ba6ab8162373d51505d1c159ba1835c80a57
tomek <tomaszpawelkowalski@gmail.com>
2022-03-28 12:37:24 +0200

    feat: Added function for import User Data.

--- pages/api/main.ts
+++ pages/api/main.ts
@@ -1,17 +1,31 @@
+import axios from 'axios';
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { UserGithub } from '../interfaces/ImportUser';
 import { mockResults } from '../interfaces/search';
 
 
 
 
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse 
 ) {
     try {
-        res.status(200).json({ mockResults });
+        const data = await getExternalAPIGitHub();
+        console.log(data);
+        res.status(200).json({data});
     } catch (err:any) {
         res.status(500).json({ message: err.message })
     }
+}
+
+export async function getExternalAPIGitHub() {
+  // Fetch data from external API
+    const res = await axios.get('https://api.github.com/users/octocat');
+    const user: UserGithub = res.data;
+
+
+  // Pass data to the page via props
+  return { user }
 }
