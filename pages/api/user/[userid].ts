51cb6027a6096b8ef15b9e93b9091fbe74900096
tomek <tomaszpawelkowalski@gmail.com>
2022-03-29 14:41:50 +0200

    Feat: Add function to user endpoint.

--- pages/api/user/[userid].ts
+++
@@ -1,7 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-
-
-export default function handler(req: NextApiRequest, res:NextApiResponse) {
-  const { userid } = req.query
-  res.end(`Post: ${userid}`)
-}
