a9fd48552d8a5a6282801d26d8816d10f93f9941
tomek <tomaszpawelkowalski@gmail.com>
2022-05-16 21:43:14 +0200

    refactor: move url user github api to env file

--- pages/api/services/user.services.ts
+++ pages/api/services/user.services.ts
@@ -5,66 +5,76 @@ import { UserGithub } from "../../interfaces/ImportUser";
 import { Result } from "../../interfaces/search";
 
 export async function getExternalUsersFromGitHub(query: string) {
-
-  try {
-
-    const res = await axios.get('https://api.github.com/search/users', {
-      params: { q: query, per_page: 5, }
-      , headers: { Authorization: `token ${process.env.gitToken}` }
-    });
-    const data: GeneralUserList = res.data;
-    // console.log(data);
-    return data as GeneralUserList;
-  } catch (error: any) {
-    console.error(error);
-    throw new Error(error);
-  }
+	try {
+		const res = await axios.get(<string>process.env.UsersSearchUrl, {
+			params: { q: query, per_page: 5 },
+			headers: { Authorization: `token ${process.env.gitToken}` },
+		});
+		const data: GeneralUserList = res.data;
+		// console.log(data);
+		return data as GeneralUserList;
+	} catch (error: any) {
+		console.error(error);
+		throw new Error(error);
+	}
 }
-
 
 export async function GetMoreUserData(BasicsUsers: GeneralUserList) {
-  let promises = [];
-  let user: UserGithub[] = [];
-  promises = BasicsUsers.items.map((user) => { return axios.get('https://api.github.com/users/' + user.login, { headers: { Authorization: `token ${process.env.gitToken}` } }); })
+	let promises = [];
+	let user: UserGithub[] = [];
+	promises = BasicsUsers.items.map((user) => {
+		return axios.get(process.env.UserUrl + user.login, {
+			headers: { Authorization: `token ${process.env.gitToken}` },
+		});
+	});
 
-  await Promise.all(promises).then((response) => { user = response.map((res) => { return res.data }); }).catch(error => { console.error(error); throw new Error(error); });
+	await Promise.all(promises)
+		.then((response) => {
+			user = response.map((res) => {
+				return res.data;
+			});
+		})
+		.catch((error) => {
+			console.error(error);
+			throw new Error(error);
+		});
 
-  return user;
-
-};
-
+	return user;
+}
 
 export function MappingUserOutPutData(Users: UserGithub[]): Result[] {
-  const data: Result[] = [];
+	const data: Result[] = [];
 
-  Users.forEach(user => {
-    const result: Result = {
-      id: user.id,
-      login: user.login,
-      avatarURL: user.avatar_url,
-      fullName: user.name,
-      bio: user.bio,
-      location: user.location,
-      followers: user.followers,
-      following: user.following,
-      // starred: user.starredURL,
-    };
-    data.push(result);
-  });
+	Users.forEach((user) => {
+		const result: Result = {
+			id: user.id,
+			login: user.login,
+			avatarURL: user.avatar_url,
+			fullName: user.name,
+			bio: user.bio,
+			location: user.location,
+			followers: user.followers,
+			following: user.following,
+			// starred: user.starredURL,
+		};
+		data.push(result);
+	});
 
-
-  return data;
+	return data;
 }
 async function getGitHubStarred(user: string) {
-  const response = await axios.get(`https://api.github.com/users/${user}/starred`, {
-    headers: { Authorization: `token ${process.env.gitToken}` }, params: { per_page: 100, }
-  });
-  const data: IStars[] = response.data;
-  console.log('response', data);
-  //   for (d of)
-  return data.length;
-  // stargazers_count
-
+	const response = await axios.get(
+		process.env.UserDetailsUrl + `${user}/starred`,
+		{
+			headers: { Authorization: `token ${process.env.gitToken}` },
+			params: { per_page: 100 },
+		}
+	);
+	const data: IStars[] = response.data;
+	console.log("response", data);
+	//   for (d of)
+	return data.length;
+	// stargazers_count
 }
 
 export async function UserData(query: string): Promise<Result[]> {
