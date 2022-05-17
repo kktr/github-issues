24879018ca399972e0cf783f8fad182901ca7fcb
tomek <tomaszpawelkowalski@gmail.com>
2022-05-17 18:30:11 +0200

    refactor: handle data at endpoint

--- pages/api/main.ts
+++ pages/api/main.ts
@@ -1,32 +1,29 @@
 
 import type { NextApiRequest, NextApiResponse } from "next";
-import { RepositoryData } from './services/repository.services';
-import { UserData } from './services/user.services';
+import { Result } from "../interfaces/search";
+import { UserData } from "./services/user.services";
 
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+	try {
+		let combineData = await getGithubData(req);
+		// const query = req.query.search as string;
 
-
-
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  const search = req.query.search as string;;
-  try {
-    if (typeof search !== 'string') { res.status(200).json({}); };
-    if (search.length < 2) throw new Error('Search string too short');
-
-
-    let MappedUserData = await UserData(search);
-    let MappedRepoData = await RepositoryData(search);
-
-    let combineData = MappedUserData.concat(MappedRepoData);
-
-    res.status(200).json(combineData);
-  } catch (err: any) {
-    res.status(500).json({ message: err.message })
-  }
+		return res.status(200).json(combineData);
+	} catch (err: any) {
+		res.status(500).json({ message: err.message });
+	}
 }
 
+async function getGithubData(req: NextApiRequest): Promise<Result[]> {
+	const search = req.query.search as string;
 
+	if (search?.length < 2) throw new Error("Search string too short");
 
+	let MappedUserData = await UserData(search);
+	// let MappedRepoData = await RepositoryData(search);
+
+	// let combineData = MappedUserData.concat(MappedRepoData);
+	// let combineData = MappedUserData.concat(MappedRepoData);
+	return MappedUserData;
+}
 
