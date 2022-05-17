62c4e8ccd9d73aeac0bcc8edea36432af2e8366a
tomek <tomaszpawelkowalski@gmail.com>
2022-05-17 18:31:02 +0200

    feat: new interface for users list not from query

--- pages/interfaces/importListUsers.ts
+++ pages/interfaces/importListUsers.ts
@@ -1,27 +1,32 @@
-export interface GeneralUserList {
-    totalCount:        number;
-    incompleteResults: boolean;
-    items:             GeneralUser[];
+export interface IUser {
+	login?: string;
+	id?: number;
+	node_id?: string;
+	avatar_url?: string;
+	gravatar_id?: string;
+	url?: string;
+	html_url?: string;
+	followers_url?: string;
+	following_url?: string;
+	gists_url?: string;
+	starred_url?: string;
+	subscriptions_url?: string;
+	organizations_url?: string;
+	repos_url?: string;
+	events_url?: string;
+	received_events_url?: string;
+	type?: Type;
+	site_admin?: boolean;
 }
 
-export interface GeneralUser {
-    login:             string;
-    id:                number;
-    nodeID:            string;
-    avatarURL:         string;
-    gravatarID:        string;
-    url:               string;
-    htmlURL:           string;
-    followersURL:      string;
-    subscriptionsURL:  string;
-    organizationsURL:  string;
-    reposURL:          string;
-    receivedEventsURL: string;
-    type:              string;
-    score:             number;
-    followingURL:      string;
-    gistsURL:          string;
-    starredURL:        string;
-    eventsURL:         string;
-    siteAdmin:         boolean;
+export enum Type {
+	Organization = "Organization",
+	User = "User",
+}
+
+export interface IUserSearchList {
+	type: "search";
+	totalCount: number;
+	incompleteResults: boolean;
+	items: IUser[];
 }
