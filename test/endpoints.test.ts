7a5001034c37484a868742b6f0a3c2d58819ce73
tomek <tomaszpawelkowalski@gmail.com>
2022-05-19 13:51:39 +0200

    test: add case  with query search  and ok status

--- test/endpoints.test.ts
+++ test/endpoints.test.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+const url = "http://localhost:3000/api/main" || <string>process.env.MainUrl;
+
 describe("Test Check", () => {
 	test("Test check", () => {
 		const n = 1;
@@ -9,8 +11,15 @@ describe("Test Check", () => {
 });
 
 describe("e2e", () => {
-	test("should return 200", async () => {
-		const response = await axios.get("http://localhost:3000/api/main");
+	test("should return ok status, without search", async () => {
+		expect(url).toBe("http://localhost:3000/api/main");
+		const response = await axios.get(url);
+		expect(response.status).toBe(200);
+	}, 30000);
+	test("should return ok status with query search", async () => {
+		const search = "test";
+		expect(url).toBe("http://localhost:3000/api/main");
+		const response = await axios.get(url + `?search=${search}`);
 		expect(response.status).toBe(200);
 	}, 30000);
 });
